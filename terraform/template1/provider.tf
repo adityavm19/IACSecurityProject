6a3726d4559c436e2145b1fbde8c02f5aebeeec9
Aditya More <adityavm19@gmail.com>
2025-07-26 20:41:45 +0100

    Pool Update2 .yml file

--- terraform/template1/provider.tf
+++ terraform/template1/provider.tf
@@ -1,5 +1,21 @@
+# Configure the Azure provider, you can have many
+# if you use azurerm provider, it's source is hashicorp/azurerm
+# short for registry.terraform.io/hashicorp/azurerm
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.8.0"
+    }
+  }
+
+  required_version = ">= 1.9.0"
+}
+# configures the provider
+
 provider "azurerm" {
   features {}
-
-  subscription_id = "714bef35-97c9-41ae-9498-96d07a42ea60"
+  
 }
